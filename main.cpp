3d30bb5093bd2bd9bdc9c3d852ce66f0a217b4d6
Divesh Kamble <diveshkamble@gmail.com>
2022-08-20 14:19:36 +0530

    Testing

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main()
 {
 	int TreeKeys[16] = {50, 76, 23, 4, 32, 64, 15, 52, 14, 100, 83, 2, 3, 70, 87, 80};
+	int input = 0;
 	BST myTree;
 
 	cout << "Printing the tree in order \nBefore adding numbers\n";
@@ -23,5 +24,22 @@ int main()
 	cout << endl;
 
 	cout << "The smallest value in the tree is " << myTree.FindSmallest() << "\n";
+
+	cout << "Enter a key value to delete. Enter -1 to stop the process.\n";
+	while (input != -1)
+	{
+		cout << "Delete node: ";
+		cin >> input;
+		{
+			if (input != -1)
+			{
+				cout << endl;
+				myTree.RemoveNode(input);
+				myTree.PrintInOrder();
+				cout << endl;
+			}
+		}
+	}
+
 	return 0;
 }
